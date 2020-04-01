863139cd08a7128749fbdf10b69712d754be769a
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-01 15:55:06 +0530

    Add Panchayat Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_101225) do
+ActiveRecord::Schema.define(version: 2020_04_01_102439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2020_04_01_101225) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["phone"], name: "index_contacts_on_phone", unique: true
+  end
+
+  create_table "panchayats", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
