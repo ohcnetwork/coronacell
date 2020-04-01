951105fdf74559e14dbdb34fdfa5ca73e1331e38
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-01 16:14:39 +0530

    Medical Needs are Taken

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :contacts, except: [:destroy] do
+    resources :non_medical_reqs do
+      get :fullfill, on: :member
+    end
+    resources :medical_reqs do
+      get :fullfill, on: :member
+    end
+  end
 end
