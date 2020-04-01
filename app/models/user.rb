d988d88eae34675e6ffa3b4d9a4a5baa523fe6b8
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-01 16:50:23 +0530

    Add Role to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: {phone_caller: "phone_caller", district_admin: "district_admin", panchayat_admin: "panchayat_admin" }
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :phone_caller
+  end
 end
