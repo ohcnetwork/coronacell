0abc0ee6dc38c2690e86d767774941eafb75b49e
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-03 00:01:29 +0530

    Add To Fields

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
   enum gender: {male: "male", female: "female"}
+  enum ration_type: {yellow: 'yellow', pink: 'pink', blue: 'blue', white: 'white'}
 
   belongs_to :panchayat
   has_many :non_medical_reqs
