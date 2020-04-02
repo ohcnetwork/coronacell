e6d6ca99983acb73d0758daa7011c1a304217d85
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-02 23:53:07 +0530

    Integrate Tailwind

--- app/models/non_medical_req.rb
+++ app/models/non_medical_req.rb
@@ -1,4 +1,5 @@
 class NonMedicalReq < ApplicationRecord
-  enum requirement_type: {rice_wheat_atta: "rice_wheat_atta", vegetables_fruits: "vegetables_fruits",spices_oil: "spices_oil", cooking_gas: "cooking_gas", drinking_water: "drinking_water", household_water: "household_water", sanitary_napkins: "sanitary_napkins", baby_foods: "baby_foods"}
+  enum requirement_type: {rice_wheat_atta: 'rice_wheat_atta', vegetables_fruits: 'vegetables_fruits',spices_oil: 'spices_oil', cooking_gas: 'cooking_gas', drinking_water: 'drinking_water', household_water: 'household_water', sanitary_napkins: 'sanitary_napkins', baby_foods: 'baby_foods'}
+
   belongs_to :contact
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "admin@gmail.com", password: "pass123", password_confirmation: "pass123", role: :admin)
+pan1 = Panchayat.create(name: "Pan 1")
+pan2 = Panchayat.create(name: "Pan 2")
+User.create(email: "pan1@gmail.com", password: "pass123", password_confirmation: "pass123", role: :panchayat_admin, panchayat_id: pan1.id)
