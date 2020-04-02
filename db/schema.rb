b86e7456b637adb5d91b5d2945a803e03211e014
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-02 23:17:20 +0530

    Add Requirement Type to Medical Needs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_122532) do
+ActiveRecord::Schema.define(version: 2020_04_02_174139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_122532) do
     t.boolean "fullfilled"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "requirement_type"
     t.index ["contact_id"], name: "index_medical_reqs_on_contact_id"
   end
 
@@ -45,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_122532) do
     t.boolean "fullfilled"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "requirement_type"
     t.index ["contact_id"], name: "index_non_medical_reqs_on_contact_id"
   end
 
