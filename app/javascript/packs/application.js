8dd309b40e8cd915ba8a7a0167d97e62e0c792f2
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-03 07:00:09 +0530

    Add panchayat Dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,11 @@ require("channels")
 // Import Tailwind
 import '../css/application.css'
 
+// Add Choices Dropdown
+const Choices = require('choices.js')
+document.addEventListener("turbolinks:load", function() {
+    var dropDownSelects = new Choices('#dropdown-choice-select')
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
