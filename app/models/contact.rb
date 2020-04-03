7a1590dbe7153cc410a477b3ea453ce2b9cf47de
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-03 11:49:15 +0530

    Add Others to Models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   enum gender: {male: "male", female: "female"}
   enum ration_type: {yellow: 'yellow', pink: 'pink', blue: 'blue', white: 'white'}
-  enum tracking_type: {flight_passenger: 'flight_passenger', domestic_passenger: 'domestic_passenger'}
+  enum tracking_type: {flight_passenger: 'flight_passenger', domestic_passenger: 'domestic_passenger', other: "other"}
 
   belongs_to :panchayat
   has_many :non_medical_reqs
