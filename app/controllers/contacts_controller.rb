32a962eed735276539f5cd514cc446f45cf0cbb3
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-04 15:57:39 +0530

    Make HomeScreen Work

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,12 +4,7 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.json
   def index
-    ###########################################################################################################################
-    # unscoped_non_medical = Contact.joins(:non_medical_reqs).joins(:medicaL_reqs).where(non_medical_reqs: {fullfilled: nil}) #
-    # unscoped_medical = Contact.joins(:non_medical_reqs).joins(:medical_reqs).where(medical_reqs: {fullfilled: nil})         #
-    # unscoped_contacts = unscoped_medical.or(unscoped_non_medical)                                                           #
-    ###########################################################################################################################
-    unscoped_contacts = Contact.all
+    unscoped_contacts = Contact.left_joins(:non_medical_reqs, :medical_reqs).where(non_medical_reqs: {fullfilled: nil}).or(Contact.left_joins(:non_medical_reqs, :medical_reqs).where(medical_reqs: {fullfilled: nil}))
     @contacts = scope_access(unscoped_contacts)
     if current_user.phone_caller?
       contacts_called_by_user_today = Contact.joins(:calls).where(calls: {user_id: current_user.id, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day}).distinct
