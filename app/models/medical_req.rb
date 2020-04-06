6d67353eb9ffb60467c5327f2be352a90a7243f8
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-06 13:34:10 +0530

    Change Naming

--- app/models/medical_req.rb
+++ app/models/medical_req.rb
@@ -1,5 +1,5 @@
 class MedicalReq < ApplicationRecord
   enum requirement_type: {diabetes: "diabetes", blood_pressure: "blood_pressure", cholestrol: "cholestrol", palliative: "palliative", asthma: "asthma", copd: "copd", cancer: "cancer", dialysis: "dialysis", ayurveda: "ayurveda", homeo: "homeo", others: "others"}
-  enum not_able_type: {undeliverable: "undeliverable", no_fund: "no_fund", no_stock: "no_stock"}
+  enum not_able_type: {undeliverable: "undeliverable", other_reasons: "other_reasons", no_stock: "no_stock"}
   belongs_to :contact
 end
